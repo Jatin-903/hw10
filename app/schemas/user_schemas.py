464b15d1118146f1a52dd64567b975b5cd11836c
Jatin Beeram <jatinbeeram@Jatins-MacBook-Air.local>
2025-04-23 18:48:29 -0400

    initial commit

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,11 +1,8 @@
-from builtins import ValueError, any, bool, str
 from pydantic import BaseModel, EmailStr, Field, validator, root_validator
 from typing import Optional, List
-from datetime import datetime
 from enum import Enum
 import uuid
 import re
-
 from app.utils.nickname_gen import generate_nickname
 
 class UserRole(str, Enum):
@@ -17,7 +14,7 @@ class UserRole(str, Enum):
 def validate_url(url: Optional[str]) -> Optional[str]:
     if url is None:
         return url
-    url_regex = r'^https?:\/\/[^\s/$.?#].[^\s]*$'
+    url_regex = r'^https?:\/\/[\w\.-]+(?:\.[\w\.-]+)+(?:[\w\-\._~:/?#\[\]@!\$&\'()*\+,;=.]+)?$'
     if not re.match(url_regex, url):
         raise ValueError('Invalid URL format')
     return url
@@ -44,50 +41,26 @@ class UserBase(BaseModel):
         from_attributes = True
 
 class UserCreate(UserBase):
-    email: EmailStr = Field(..., example="john.doe@example.com")
-    password: str = Field(..., example="Secure*1234")
+    password: str
 
 class UserUpdate(UserBase):
-    email: Optional[EmailStr] = Field(None, example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example="john_doe123")
-    first_name: Optional[str] = Field(None, example="John")
-    last_name: Optional[str] = Field(None, example="Doe")
-    bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
-    profile_picture_url: Optional[str] = Field(None, example="https://example.com/profiles/john.jpg")
-    linkedin_profile_url: Optional[str] =Field(None, example="https://linkedin.com/in/johndoe")
-    github_profile_url: Optional[str] = Field(None, example="https://github.com/johndoe")
-
     @root_validator(pre=True)
-    def check_at_least_one_value(cls, values):
-        if not any(values.values()):
-            raise ValueError("At least one field must be provided for update")
+    def at_least_one_non_null(cls, values):
+        if not any(v is not None for v in values.values()):
+            raise ValueError("At least one non-null field must be provided for update")
         return values
 
 class UserResponse(UserBase):
-    id: uuid.UUID = Field(..., example=uuid.uuid4())
-    role: UserRole = Field(default=UserRole.AUTHENTICATED, example="AUTHENTICATED")
-    email: EmailStr = Field(..., example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=generate_nickname())    
-    role: UserRole = Field(default=UserRole.AUTHENTICATED, example="AUTHENTICATED")
-    is_professional: Optional[bool] = Field(default=False, example=True)
+    id: uuid.UUID
+    role: UserRole = Field(default=UserRole.AUTHENTICATED)
+    is_professional: Optional[bool] = Field(default=False)
 
 class LoginRequest(BaseModel):
-    email: str = Field(..., example="john.doe@example.com")
-    password: str = Field(..., example="Secure*1234")
-
-class ErrorResponse(BaseModel):
-    error: str = Field(..., example="Not Found")
-    details: Optional[str] = Field(None, example="The requested resource was not found.")
+    email: EmailStr = Field(..., example="test@example.com")
+    password: str = Field(..., example="MyPass123")
 
 class UserListResponse(BaseModel):
-    items: List[UserResponse] = Field(..., example=[{
-        "id": uuid.uuid4(), "nickname": generate_nickname(), "email": "john.doe@example.com",
-        "first_name": "John", "bio": "Experienced developer", "role": "AUTHENTICATED",
-        "last_name": "Doe", "bio": "Experienced developer", "role": "AUTHENTICATED",
-        "profile_picture_url": "https://example.com/profiles/john.jpg", 
-        "linkedin_profile_url": "https://linkedin.com/in/johndoe", 
-        "github_profile_url": "https://github.com/johndoe"
-    }])
-    total: int = Field(..., example=100)
-    page: int = Field(..., example=1)
-    size: int = Field(..., example=10)
+    items: List[UserResponse]
+    total: int
+    page: int
+    size: int
